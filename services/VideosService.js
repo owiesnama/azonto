5eeac701a62bf24ce3ba29701a277ae28052e083
Musab Khunaijir <musabkhunaijir@gmail.com>
2019-04-01 19:45:39 +0200

    add trending videos

--- services/VideosService.js
+++ services/VideosService.js
@@ -3,6 +3,7 @@ const i18n = require('i18n');
 const BaseService = require('./BaseService');
 const models = require('../DB/models/index');
 const VideosDao = require('../DAOs/VideosDao');
+const constants = require('../config/constants');
 
 const videosModel = models.videos;
 
@@ -36,6 +37,41 @@ class VideosService extends BaseService {
     return new Promise(async (resolve, reject) => {
       try {
         const result = await new VideosDao().findAll(where, pageSize, pageNumber, orderBy)
+        resolve(result);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  trending() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const _sequelize = models.sequelize;
+
+        // get the videos views summation 
+        const sum = await _sequelize.query('SELECT sum(views) as sum FROM videos', {
+          type: _sequelize.QueryTypes.SELECT
+        });
+
+        // gets the sum value from the sum[] array
+        const sumValue = sum[0].sum;
+
+        // calc the 25% of viewed videos
+        const quarter = (sumValue * .25);
+
+        console.log('sum: '.green, JSON.stringify(sumValue));
+        console.log('quarter: '.green, (quarter));
+
+        // return the top 10 trending videos 
+        const result = await new VideosDao().findAll({
+          status_id: constants.APPROVED,
+          views: {
+            [_sequelize.Op.gt]: quarter
+          }
+        }, 10, 0, [
+          ['views', 'DESC']
+        ])
         resolve(result);
       } catch (error) {
         reject(error);
